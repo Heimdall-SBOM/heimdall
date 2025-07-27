79798c579e37728a9414721722aa773c9d41822e
Trevor Bakker <trevor.bakker@uta.edu>
2025-07-26 22:24:09 -0500

    Removed extraneous files

--- test_gcov_minimal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-dir="build/src/CMakeFiles/heimdall-core.dir/common"
-cd "$dir"
-gcov -o . MetadataExtractor.cpp.gcda /home/tbakker/code/heimdall/src/common/MetadataExtractor.cpp
-ls -l *.gcov 
